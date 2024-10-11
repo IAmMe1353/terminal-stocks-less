ccccf0bfcada6792a36d5f821a28ada3488ba407
IAmMe1353 <96355765+IAmMe1353@users.noreply.github.com>
2024-10-10 19:29:12 -0700

    Update yahooService.js

--- app/services/yahooService.js
+++ app/services/yahooService.js
@@ -61,11 +61,20 @@ function getCurrentPrice(tickers) {
         console.log("hweelpl hweelpl hahaha");
         const consoleConst = console.log;
         const consoleWarn = console.warn;
+        const errorlog = console.error;
+        const infolog = console.info;
+        const debuglog = console.debug;
         console.log = () => {};
         console.warn = () => {};
+        console.error = () => {};
+        console.info = () => {};
+        console.debug = () => {};
         var entity = await yahooFinance.quote(ticker);
         console.log  = consoleConst;
-         console.warn = consoleWarn;
+        console.warn = consoleWarn;
+        console.error = errorlog;
+        console.info = infolog;
+        console.debug = debuglog;
         console.log("hweelpl hweelpl ");
         // Restore console.log and console.warn
 
